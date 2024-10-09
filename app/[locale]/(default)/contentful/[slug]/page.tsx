fcd2c8c2aa7ee26d0df36c8979b87d73db1d2cc8
Vijay Khirade <vijay.khirade@teamwass.com>
2024-10-09 11:27:47 +0530

    RichImageComponent use on blog details page

--- app/[locale]/(default)/contentful/[slug]/page.tsx
+++ app/[locale]/(default)/contentful/[slug]/page.tsx
@@ -30,6 +30,12 @@ const GetBlogPostsQuery = contentfulGraphql(`
         publishedDate,
         author{
         name,
+        avatar{
+          url
+          title
+          height
+          width
+        }
         },
         relatedBlogPostsCollection{
           items{
@@ -44,6 +50,51 @@ const GetBlogPostsQuery = contentfulGraphql(`
             }
         }
         content{
+          links{
+            entries{
+              inline {
+                sys {
+                  id
+                }
+                __typename
+                ... on  PageBlogPost{
+                  title
+                  slug
+                }
+              }
+              block{
+                sys{
+                  id
+                }
+                ... on ComponentRichImage{
+                  image{
+                    url
+                    description
+                    title
+                    fileName
+                    width
+                    height
+                  }
+                }
+              }
+            }
+            assets {
+              block {
+                sys {
+                  id
+                }
+                __typename
+                ... on Asset{
+                  url
+                  title
+                  description
+                  size
+                  width
+                  height
+                }
+              }
+            }
+          }
           json,
         },
         shortDescription,
@@ -70,12 +121,19 @@ const variables = {
         <div className="prose mt-8 border-t pt-8" key={post?.sys.id}>
            <h1 className="mb-2 text-3xl font-black lg:text-5xl">{post?.title}</h1>
            <div className="mb-8 flex">
-              <small className="mb-0 text-base text-gray-500">
+              <small className="mb-0 text-base text-gray-500 pt-5">
                   {"Publised On :"}{format.dateTime(new Date(post.publishedDate))}
               </small>
               {Boolean(post.author) && (
-                <small className="text-base text-gray-500">, by {post.author.name}</small>
+                <small className="text-base text-gray-500 pt-5">, by {post.author.name}</small>
               )}
+              {Boolean(post.author) && <Image
+              alt={post.name?.name ?? ''}
+              height={100}
+              src={post.author.avatar?.url ?? ''}
+              width={100}
+              className='pl-5'
+            />}
             </div>
           <span className='prose mt-8 border-t pt-8 '> <br/></span>
             <Image
@@ -84,8 +142,8 @@ const variables = {
               src={post?.featuredImage?.url ?? ''}
               width={1500}
             />
-          <h3 className='prose mt-8 border-t pt-8'>{post?.shortDescription}</h3>
-          <RichText document={post?.content.json} />
+          <h3 className='mb-4 text-xl font-bold md:text-2xl mt-8 border-t pt-8'>{post?.shortDescription}</h3>
+          <RichText document={post?.content} />
           <div className="mb-10 flex mt-8 border-t pt-8">
           {post.relatedBlogPostsCollection.items ? (
              <h1 className="mb-2 text-3xl font-black lg:text-3xl">{"Related Pages :"}</h1>
--- components/contentful/RichText.tsx
+++ components/contentful/RichText.tsx
@@ -1,18 +1,111 @@
-import { Document as RichTextDocument } from '@contentful/rich-text-types'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
-type RichTextProps = {
-	document: RichTextDocument | null
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
+import Image from 'next/image';
+// Create a bespoke renderOptions object to target BLOCKS.EMBEDDED_ENTRY (linked block entries e.g. code blocks)
+// INLINES.EMBEDDED_ENTRY (linked inline entries e.g. a reference to another page blog post)
+// and BLOCKS.EMBEDDED_ASSET (linked assets e.g. images)
+
+function renderOptions(links) {
+  // create an asset map
+  const assetMap = new Map();
+  // loop through the assets and add them to the map
+  for (const asset of links.assets.block) {
+    assetMap.set(asset.sys.id, asset);
+  }
+
+  // create an entry map
+  const entryMap = new Map();
+  // loop through the block linked entries and add them to the map
+  for (const entry of links.entries.block) {
+    entryMap.set(entry.sys.id, entry);
+  }
+
+   // loop through the inline linked entries and add them to the map
+  for (const entry of links.entries.inline) {
+    entryMap.set(entry.sys.id, entry);
+  }
+
+  return {
+    // other options...
+
+    renderNode: {
+      // other options...
+      [INLINES.EMBEDDED_ENTRY]: (node, children) => {
+        // find the entry in the entryMap by ID
+        const entry = entryMap.get(node.data.target.sys.id);
+        // render the entries as needed
+        if (entry.__typename === "PageBlogPostContentLinks") {
+			    return (
+				    <img src={entry.block.image.url} alt="My image alt text" />
+			    );
+        }
+      },
+	  [BLOCKS.EMBEDDED_ENTRY]: (node, children) => {
+      // find the entry in the entryMap by ID
+        const entry = entryMap.get(node.data.target.sys.id);
+        if (entry.__typename === "ComponentRichImage") {
+          return (
+            <Image
+                alt={entry?.image.title || 'No description'}
+                height={entry.image.height}
+                src={entry?.image.url || '/placeholder.png'} // Add a fallback URL for the image
+                width={entry.image.width}
+                className="h-full w-full box-border"
+              />
+            );
+        }
+        // render the entries as needed by looking at the __typename
+        // referenced in the GraphQL query
+        if (entry.__typename === "CodeBlock") {
+          return (
+            <pre>
+              <code>{entry.code}</code>
+            </pre>
+          );
+        }
+
+        if (entry.__typename === "VideoEmbed") {
+          return (
+            <iframe
+            src={entry.embedUrl}
+            height="100%"
+            width="100%"
+            frameBorder="0"
+            scrolling="no"
+            title={entry.title}
+            allowFullScreen={true}
+            />
+          );
+        }
+      },
+
+      [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
+        // find the asset in the assetMap by ID
+        const asset = assetMap.get(node.data.target.sys.id);
+
+        // render the asset accordingly
+        return (
+          <Image src={asset.url} className="h-full w-full bg-teal-400" alt="My image alt text" />
+        );
+      },
+
+	  [BLOCKS.PARAGRAPH]: (node, children) => {
+        // find the asset in the assetMap by ID
+       // const asset = assetMap.get(node.data.target.sys.id);
+
+        // render the asset accordingly
+        return (
+          <p className="pt-5 pb-5 tracking-wide">{children}</p>
+        );
+      },
+    },
+  };
 }
 
+// Render pageBlogPost.content.json to the DOM using
+// documentToReactComponents from "@contentful/rich-text-react-renderer"
 
-function RichText({ document }: RichTextProps) {
-	if (!document) {
-		return null
-	}
-
-	//return <>{documentToHtmlString(document)}</>
-	return <>{documentToReactComponents(document)}</>
+export default function RichText(props) {
+  const { document } = props;
+  return <>{documentToReactComponents(document.json, renderOptions(document.links))}</>;
 }
-
-export default RichText
